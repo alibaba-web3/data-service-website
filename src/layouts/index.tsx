5562d9b6a7573f868e8c17701df3297e1f8429ed
mianyun.yt <mianyun.yt@alibaba-inc.com>
2023-02-24 17:02:39 +0800

    feat: 数据下载

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,15 +5,7 @@ export default function Layout() {
   return (
     <div className={styles.navs}>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
+       
       </ul>
       <Outlet />
     </div>
