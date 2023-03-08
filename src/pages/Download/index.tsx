4c0ad71cc08ec98155dcdabcebf65ffd30edeec9
yofine <yofineliu@gmail.com>
2023-03-08 23:22:44 +0800

    feat: refine download page

--- src/pages/Download/index.tsx
+++ src/pages/Download/index.tsx
@@ -1,25 +1,41 @@
-export default function Download() {
-  return (
-    <div>
-      <h2>回测数据下载</h2>
+import { PageContainer } from '@ant-design/pro-components';
+import { Card, Typography } from 'antd';
+import React from 'react';
 
-      <a href="http://8.222.145.211:8082/api/download/spot/1d" target="_blank">
-        Binance 现货价格-日线
-      </a>
-      <br />
-      <br />
-      <a href="http://8.222.145.211:8082/api/download/tvl/1d" target="_blank">TVL</a>
-      <br />
-      <br />
-      {/* <a href="http://8.222.145.211:8082/api/download/erc20/user/1d">合约调用次数</a>
+const { Link } = Typography;
+
+const Download: React.FC = () => {
+  return (
+    <PageContainer>
+      <Card title="回测数据下载">
+        <h4>
+          <Link
+            href="http://8.222.145.211:8082/api/download/spot/1d"
+            target="_blank"
+          >
+            Binance 现货价格-日线
+          </Link>
+        </h4>
+        <h4>
+          <Link
+            href="http://8.222.145.211:8082/api/download/tvl/1d"
+            target="_blank"
+          >
+            TVL
+          </Link>
+        </h4>
+        {/* <a href="http://8.222.145.211:8082/api/download/erc20/user/1d">合约调用次数</a>
         <br />
         <br /> */}
-      <div>
-        更多数据上线中。提出你的数据诉求，更快上线:{' '}
-        <a href="https://alidocs.dingtalk.com/i/nodes/3QD5Ea7xAo4VEXqz1B9rWG1YBwgnNKb0?iframeQuery=utm_source%3Dportal%26utm_medium%3Dportal_recent">
-          反馈链接
-        </a>
-      </div>
-    </div>
+        <h4>
+          更多数据上线中。提出你的数据诉求，更快上线：
+          <Link href="https://alidocs.dingtalk.com/i/nodes/3QD5Ea7xAo4VEXqz1B9rWG1YBwgnNKb0?iframeQuery=utm_source%3Dportal%26utm_medium%3Dportal_recent">
+            反馈链接
+          </Link>
+        </h4>
+      </Card>
+    </PageContainer>
   );
-}
+};
+
+export default Download;
