60133e637cfbd5185fc5f36c2b411d0b6a781de7
fuxian <liuwang.liu@alibaba-inc.com>
2023-03-08 17:06:04 +0800

    feat: init

--- .umirc.ts
+++ .umirc.ts
@@ -9,26 +9,5 @@ export default defineConfig({
   layout: {
     title: '@umijs/max',
   },
-  routes: [
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      name: '首页',
-      path: '/home',
-      component: './Home',
-    },
-    {
-      name: '权限演示',
-      path: '/access',
-      component: './Access',
-    },
-    {
-      name: ' CRUD 示例',
-      path: '/table',
-      component: './Table',
-    },
-  ],
   npmClient: 'pnpm',
 });
