6d25cf3f7242d72732c377084534cff9a6ea7918
fuxian <liuwang.liu@alibaba-inc.com>
2023-03-13 16:17:25 +0800

    feat: download page

--- src/pages/Download/index.tsx
+++ src/pages/Download/index.tsx
@@ -1,38 +1,115 @@
+import { DownloadOutlined } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-components';
-import { Card, Typography } from 'antd';
+import { Button, Card, List, Tabs } from 'antd';
 import React from 'react';
 
-const { Link } = Typography;
+const { TabPane } = Tabs;
+
+const tabData = [
+  {
+    key: '1',
+    title: '量化回测数据',
+  },
+  {
+    key: '2',
+    title: '其他数据',
+  },
+];
+
+interface ListDataType {
+  tab: string;
+  title: string;
+  url: string;
+}
+
+function renderTabBar(props: any, DefaultTabBar: any) {
+  return (
+    <div style={{ padding: '0, 24px' }}>
+      <DefaultTabBar {...props} />
+    </div>
+  );
+}
 
 const Download: React.FC = () => {
+  const listData = [
+    {
+      tab: '1',
+      title: 'Binance 现货价格-日线',
+      url: 'http://8.222.145.211:8082/api/download/spot/1d',
+    },
+    {
+      tab: '1',
+      title: 'TVL',
+      url: 'http://8.222.145.211:8082/api/download/tvl/1d',
+    },
+    {
+      tab: '1',
+      title: '合约调用次数',
+      url: 'http://8.222.145.211:8082/api/download/erc20/user/1d',
+    },
+    {
+      tab: '1',
+      title: '项目利润数据',
+      url: 'http://8.222.145.211:8082/api/download/profit/1d',
+    },
+    {
+      tab: '2',
+      title: 'Coming Soon',
+      url: 'https://test1.com',
+    },
+  ];
+
+  /**
+   * 渲染下载列表数据
+   * @param key
+   * @param listData
+   */
+  const renderListData = (key: string, listData: ListDataType[]) => {
+    return listData.filter((item) => item.tab === key);
+  };
+
   return (
     <PageContainer>
-      <Card title="回测数据下载">
-        <h4>
-          <Link
-            href="http://8.222.145.211:8082/api/download/spot/1d"
-            target="_blank"
-          >
-            Binance 现货价格-日线
-          </Link>
-        </h4>
-        <h4>
-          <Link
-            href="http://8.222.145.211:8082/api/download/tvl/1d"
-            target="_blank"
-          >
-            TVL
-          </Link>
-        </h4>
-        {/* <a href="http://8.222.145.211:8082/api/download/erc20/user/1d">合约调用次数</a>
-        <br />
-        <br /> */}
-        <h4>
-          更多数据上线中。提出你的数据诉求，更快上线：
-          <Link href="https://alidocs.dingtalk.com/i/nodes/3QD5Ea7xAo4VEXqz1B9rWG1YBwgnNKb0?iframeQuery=utm_source%3Dportal%26utm_medium%3Dportal_recent">
-            反馈链接
-          </Link>
-        </h4>
+      <Card
+        title={
+          <div>
+            已有数据开放下载 👇🏻 &nbsp; 反馈你 &nbsp;
+            <a
+              href={
+                'https://alidocs.dingtalk.com/i/nodes/3QD5Ea7xAo4VEXqz1B9rWG1YBwgnNKb0?iframeQuery=utm_source%3Dportal%26utm_medium%3Dportal_recent'
+              }
+            >
+              想要的数据 &nbsp;
+            </a>
+            更快上线 🔥🔥
+          </div>
+        }
+      >
+        <Tabs
+          defaultActiveKey="1"
+          renderTabBar={renderTabBar}
+          tabPosition="left"
+          style={{ height: 620 }}
+        >
+          {tabData.map((tab) => (
+            <TabPane tab={tab.title} key={tab.key}>
+              <List
+                bordered
+                dataSource={renderListData(tab.key, listData)}
+                renderItem={(item) => (
+                  <List.Item>
+                    <List.Item.Meta title={item.title} />
+                    <Button
+                      type="primary"
+                      icon={<DownloadOutlined />}
+                      href={item.url}
+                    />
+                  </List.Item>
+                )}
+              />
+            </TabPane>
+          ))}
+        </Tabs>
       </Card>
     </PageContainer>
   );
